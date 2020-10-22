866db58beacb53b62bb1bb26fb947e1c8baf07d7
Matthew Sharp <msharp01@hotmail.com>
2020-10-22 15:17:15 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A model designed to predict the future sales of a store based on location characteristics.',
+    author='Matthew Sharp',
+    license='MIT',
+)
